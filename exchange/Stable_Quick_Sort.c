dfbd4951cfebd91b5e1c6b8c06b941e98c98067d
Henrique <hss@outlook.com.br>
2020-07-04 19:52:25 -0300

    Sort Algorithms

--- exchange/Stable_Quick_Sort.c
+++ exchange/Stable_Quick_Sort.c
@@ -1,72 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void QuickSort(int array[]){   
-    /*mid = len(ar)/2;
-    pivot = ar[mid] 
-  
-    smaller,greater = [],[] 
-   
-    for(indx, val in enumerate(ar))
-        if(indx != mid)
-            if(val < pivot)
-                smaller.append(val) 
-            else if (val > pivot)
-                greater.append(val) 
-            else
-                if(indx < mid)
-                    smaller.append(val) 
-                else
-                    greater.append(val) 
-    return quickSort(smaller)+[pivot]+quickSort(greater) 
-    */
-}
-/*
+// Worst case performance O(n**2)
+// Best case performance O(n log n)
+// Average case performance O(n log n)
 
-static void OptimizedQuickSort(ref int[] Arr, int Left, int Right)
-{
-    int Pivot;
-    Pivot = Q_Sort(ref Arr, Left, Right);
-    if(Left < Pivot - 1)
-    {
-        OptimizedQuickSort(ref Arr, Left, Pivot - 1);
-    }
-    if(Right > Pivot + 1)
-    {
-        OptimizedQuickSort(ref Arr, Pivot + 1, Right);
-    }
-}  
-
-static int Q_Sort(ref int[] Arr, int Left, int Right)
-{
-    int Pivot;
-    Pivot = Arr[Left];
-    while(Left < Right)
-    {
-        while((Arr[Right] >= Pivot) && (Left < Right))
-        {
-            Right--;
+int Sort(int array[], int start, int end){
+    int aux;
+    aux = array[start];
+    while(start < end){
+        while((array[end] >= aux) && (start < end))
+            end--;
+        if(start != end){
+            array[start] = array[end];
+            start++;
         }
-        if(Left != Right)
-        {
-            Arr[Left] = Arr[Right];
-            Left++;
-        }
-        while((Arr[Left] <= Pivot) && (Left < Right))
-        {
-            Left++;
-        }
-        if(Left != Right)
-        {
-            Arr[Right] = Arr[Left];
-            Right--;
+        while((array[start] <= aux) && (start < end))
+            start++;
+        if(start != end){
+            array[end] = array[start];
+            end--;
         }
     }   
-    Arr[Left] = Pivot;
-    return Left;
+    array[start] = aux;
+    return start;
+}
+
+void StableQuickSort(int array[], int start, int end){
+    int aux;
+    aux = Sort(array, start, end);
+    if(start < aux - 1)
+        StableQuickSort(array, start, aux - 1);
+    if(end > aux + 1)
+        StableQuickSort(array, aux + 1, end);
 }  
 
-*/
 int main(){
     int array[] = {979,82,937,764,309,761,824,540,786,732},i;
     
@@ -75,7 +43,7 @@ int main(){
     
     printf("\n\n");
     
-    Quick_Sort(array,0,9);
+    StableQuickSort(array,0,9);
     
     for(i = 0; i < 10; i++)
         printf("%d ",array[i]);
